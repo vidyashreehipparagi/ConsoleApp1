31f0a548068d6158261364267337799bf0febd58
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-24 18:09:23 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i>=1; i--)
+            {
+                for (int j = 1; j<=i; j++)
+                {
+
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+
+            int n = 5, a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
